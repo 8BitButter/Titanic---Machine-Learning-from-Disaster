2b112430f4cf0bc3a52c3cd12cbe0b9cf0519fa5
Rajat <rajatyofficial@gmail.com>
2025-01-06 23:33:58 +0530

    fixed space in project name

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name = 'Titanic Project'
+project_name = 'Titanic_Project'
 
 list_of_files=[
     f"src/{project_name}/__init__.py",
