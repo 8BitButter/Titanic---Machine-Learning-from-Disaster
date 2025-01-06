cdb5b5a84194dc018f9b6cb72c8964f6fb11b3e0
Rajat <rajatyofficial@gmail.com>
2025-01-06 23:53:16 +0530

    Logging and Custom Exception file compleated

---
+++ app.py
@@ -0,0 +1,12 @@
+from src.Titanic_Project.logger import logging
+from src.Titanic_Project.exception import CustomException
+import sys
+
+
+if __name__=='__main__':
+    logging.info("The execution has started")
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
