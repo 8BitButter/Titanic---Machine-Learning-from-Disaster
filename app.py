ffe382941b0a78d5005b6d6f5201f524b888f08c
Rajat <rajatyofficial@gmail.com>
2025-01-08 20:50:58 +0530

    Compleated Data Ingestion

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from src.Titanic_Project.logger import logging
 from src.Titanic_Project.exception import CustomException
 import sys
+from src.Titanic_Project.components.data_ingestion import DataIngestion
+from src.Titanic_Project.components.data_ingestion import DataIngestionConfig
 
 
 if __name__=='__main__':
     logging.info("The execution has started")
     try:
-        a=1/0
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
--- src/Titanic_Project/components/data_ingestion.py
+++ src/Titanic_Project/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.Titanic_Project.logger import logging
 import pandas as pd
 from dataclasses import dataclass
 from src.Titanic_Project.utils import read_sql_data
+from sklearn.model_selection import train_test_split
 
 @dataclass
 class DataIngestionConfig:
@@ -24,7 +25,17 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            df.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
+            logging.info("Data ingestion is compleated")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
 
         except Exception as e:
             raise CustomException(e,sys)
