5b828bf389cda5e8d1475bf9a481167693024023
Rajat <rajatyofficial@gmail.com>
2025-01-12 22:12:24 +0530

    Data transformation upto pipelines compleated

--- src/Titanic_Project/components/data_transformation.py
+++ src/Titanic_Project/components/data_transformation.py
@@ -6,6 +6,11 @@ from src.Titanic_Project.logger import logging
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+#from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 
 @dataclass
@@ -16,13 +21,19 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
-    def get_data_transformer_obj(self):
+    def get_data_transformer_pipeline(self):
         '''
         this function is responsible for data transformation
         '''
         try:
             
-            pass
+            pipeline = Pipeline([("ageimputer",AgeImputer()),
+                    ("featureencoder",FeatureEncoder()),
+                    ("featuredropper",FeatureDropper())])
+            
+            logging.info("Pipeline compleated ")
+
+            return pipeline
 
         except Exception as e:
             raise CustomException(e,sys)
@@ -30,8 +41,54 @@ class DataTransformation:
 
     def initiate_data_transformation(self,train_path,test_path):
         try:
-            pass
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Reading test and train file.")
+
+            pipeline = self.get_data_transformer_pipeline()
+
+            input_train_df = pipeline.fit_transform(train_df)
+            input_test_df = pipeline.transform(test_df)
+
+            ### continue from here
 
         
         except Exception as e:
             raise CustomException(sys,e)
+        
+
+class AgeImputer(BaseEstimator,TransformerMixin):
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X):
+        imputer = SimpleImputer(strategy="mean")
+        X['Age'] = imputer.fit_transform(X[['Age']])
+        return X
+    
+
+class FeatureEncoder(BaseEstimator,TransformerMixin):
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X):
+        encoder = OneHotEncoder()
+        matrix = encoder.fit_transform(X[['Embarked']]).toarray()
+        column_names = ["C","S","Q","N"]
+
+        for i in range(len(matrix.T)):
+            X[column_names[i]] = matrix.T[i]
+            
+        matrix = encoder.fit_transform(X[['Sex']]).toarray()
+        column_names = ["Female","Male"]
+
+
+        for i in range(len(matrix.T)):
+            X[column_names[i]] = matrix.T[i]
+
+        return X
+    
+class FeatureDropper(BaseEstimator,TransformerMixin):
+    def fit(self,X,y=None):
+        return self
+    def transform(self,X):
+        return X.drop(["Embarked","Name","Ticket","Cabin","Sex","N"],axis=1,errors="ignore")
