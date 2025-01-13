cbd5f7bf3fd13fb4562617d8619e819b801e646d
Rajat <rajatyofficial@gmail.com>
2025-01-13 09:35:28 +0530

    compleated data transformation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src.Titanic_Project.logger import logging
 from src.Titanic_Project.exception import CustomException
 import sys
 from src.Titanic_Project.components.data_ingestion import DataIngestion
-from src.Titanic_Project.components.data_ingestion import DataIngestionConfig
+from src.Titanic_Project.components.data_transformation import DataTransformation
 
 
 if __name__=='__main__':
@@ -12,7 +12,13 @@ if __name__=='__main__':
         data_ingestion=DataIngestion()
         train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
 
+        logging.info("Data Ingestion Compleated")
+
         # Data Transformation
+        data_transformation = DataTransformation()
+        train_df,test_df = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        logging.info("Data Transformation Compleated")
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/Titanic_Project/components/data_transformation.py
+++ src/Titanic_Project/components/data_transformation.py
@@ -9,13 +9,14 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-#from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+import joblib  # For saving the pipeline
 
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifacts','preprocessor.pkl')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
 
 class DataTransformation:
     def __init__(self):
@@ -23,72 +24,85 @@ class DataTransformation:
 
     def get_data_transformer_pipeline(self):
         '''
-        this function is responsible for data transformation
+        This function creates the data transformation pipeline.
         '''
         try:
-            
-            pipeline = Pipeline([("ageimputer",AgeImputer()),
-                    ("featureencoder",FeatureEncoder()),
-                    ("featuredropper",FeatureDropper())])
-            
-            logging.info("Pipeline compleated ")
-
+            pipeline = Pipeline([
+                ("age_imputer", AgeImputer()),
+                ("feature_encoder", FeatureEncoder()),
+                ("feature_dropper", FeatureDropper())
+            ])
+            logging.info("Pipeline created successfully.")
             return pipeline
-
         except Exception as e:
-            raise CustomException(e,sys)
-        
+            raise CustomException(e, sys)
 
-    def initiate_data_transformation(self,train_path,test_path):
+    def initiate_data_transformation(self, train_path, test_path):
+        '''
+        This function applies the transformation pipeline to the train and test datasets.
+        '''
         try:
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
 
-            logging.info("Reading test and train file.")
+            logging.info("Train and test files loaded successfully.")
 
             pipeline = self.get_data_transformer_pipeline()
 
-            input_train_df = pipeline.fit_transform(train_df)
-            input_test_df = pipeline.transform(test_df)
+            train_df_transformed = pipeline.fit_transform(train_df)
+            test_df_transformed = pipeline.transform(test_df)
 
-            ### continue from here
+            # Save the pipeline
+            joblib.dump(pipeline, self.data_transformation_config.preprocessor_obj_file_path)
+            logging.info(f"Pipeline saved at {self.data_transformation_config.preprocessor_obj_file_path}")
 
-        
+            return train_df_transformed, test_df_transformed
         except Exception as e:
-            raise CustomException(sys,e)
-        
+            raise CustomException(e, sys)
 
-class AgeImputer(BaseEstimator,TransformerMixin):
-    def fit(self,X,y=None):
+
+class AgeImputer(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        self.imputer = SimpleImputer(strategy="mean")
+        self.imputer.fit(X[['Age']])
         return self
-    def transform(self,X):
-        imputer = SimpleImputer(strategy="mean")
-        X['Age'] = imputer.fit_transform(X[['Age']])
+
+    def transform(self, X):
+        X = X.copy()
+        X['Age'] = self.imputer.transform(X[['Age']])
         return X
-    
 
-class FeatureEncoder(BaseEstimator,TransformerMixin):
-    def fit(self,X,y=None):
+
+class FeatureEncoder(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        self.embarked_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+        self.sex_encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+
+        self.embarked_encoder.fit(X[['Embarked']])
+        self.sex_encoder.fit(X[['Sex']])
         return self
-    def transform(self,X):
-        encoder = OneHotEncoder()
-        matrix = encoder.fit_transform(X[['Embarked']]).toarray()
-        column_names = ["C","S","Q","N"]
 
-        for i in range(len(matrix.T)):
-            X[column_names[i]] = matrix.T[i]
-            
-        matrix = encoder.fit_transform(X[['Sex']]).toarray()
-        column_names = ["Female","Male"]
+    def transform(self, X):
+        X = X.copy()
 
+        embarked_encoded = self.embarked_encoder.transform(X[['Embarked']])
+        sex_encoded = self.sex_encoder.transform(X[['Sex']])
 
-        for i in range(len(matrix.T)):
-            X[column_names[i]] = matrix.T[i]
+        embarked_columns = self.embarked_encoder.get_feature_names_out(['Embarked'])
+        sex_columns = self.sex_encoder.get_feature_names_out(['Sex'])
+
+        for i, col in enumerate(embarked_columns):
+            X[col] = embarked_encoded[:, i]
+
+        for i, col in enumerate(sex_columns):
+            X[col] = sex_encoded[:, i]
 
         return X
-    
-class FeatureDropper(BaseEstimator,TransformerMixin):
-    def fit(self,X,y=None):
+
+
+class FeatureDropper(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
         return self
-    def transform(self,X):
-        return X.drop(["Embarked","Name","Ticket","Cabin","Sex","N"],axis=1,errors="ignore")
+
+    def transform(self, X):
+        return X.drop(columns=["Embarked", "Name", "Ticket", "Cabin", "Sex"], errors="ignore")
