3ab652929ce3d56d08b609cd9b38e4757556c3a3
Rajat <rajatyofficial@gmail.com>
2025-01-15 18:19:14 +0530

    Split data and define Models

--- src/Titanic_Project/components/model_training.py
+++ src/Titanic_Project/components/model_training.py
@@ -15,3 +15,43 @@ from xgboost import XGBRegressor
 
 from src.Titanic_Project.exception import CustomException
 from src.Titanic_Project.logger import logging
+
+@dataclass 
+class ModelTraineConfig:
+    trained_model_file_path = os.path.join("artifacts","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTraineConfig()
+
+    def initiate_model_trainer(self,train_array,test_array):
+
+        try:
+            logging.info("Split train test input data")
+
+            X_train,y_train,X_test,y_test = (
+            train_array[:,:-1],
+            train_array[:,-1],
+            test_array[:,:-1],
+            test_array[:,-1]
+            )
+
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor(),
+                "Linear Regressor":LinearRegression(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoosting Regressor":CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor":AdaBoostRegressor(),
+            }
+
+
+
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+        
