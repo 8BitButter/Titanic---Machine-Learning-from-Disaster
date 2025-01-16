f2ab7fb23bf38d2621cf0ed831b993892517982a
Rajat <rajatyofficial@gmail.com>
2025-01-16 21:25:40 +0530

    Compleated model training code

--- src/Titanic_Project/components/model_training.py
+++ src/Titanic_Project/components/model_training.py
@@ -16,6 +16,9 @@ from xgboost import XGBRegressor
 from src.Titanic_Project.exception import CustomException
 from src.Titanic_Project.logger import logging
 
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+
 @dataclass 
 class ModelTraineConfig:
     trained_model_file_path = os.path.join("artifacts","model.pkl")
@@ -23,6 +26,13 @@ class ModelTraineConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTraineConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
+
 
     def initiate_model_trainer(self,train_array,test_array):
 
@@ -45,6 +55,69 @@ class ModelTrainer:
                 "CatBoosting Regressor":CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor":AdaBoostRegressor(),
             }
+
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params)
+
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+             ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
+            print("This is the best model:")
+            print(best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model=""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
 
 
 
